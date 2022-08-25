9a2de85a95adfdb704dc788a7fefabe832532a19
cchanmi <gkfn980@hs.ac.kr>
2022-08-25 17:39:47 +0900

    pg 12947 하샤드 수

--- Algorithm/programmers/pg_12947.swift
+++ Algorithm/programmers/pg_12947.swift
@@ -13,7 +13,6 @@ func solution(_ x:Int) -> Bool {
     for i in String(x) {
         guard let number = Int(String(i)) else { break }
         sum += number
-        print(sum)
     }
-    return (sum % x == 0 ? true : false)
+    return (x % sum == 0 ? true : false)
 }
