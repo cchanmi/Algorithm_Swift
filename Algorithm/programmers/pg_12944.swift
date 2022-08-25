efea17fb66d3bc209b2fe5c811f4419336df3f57
cchanmi <gkfn980@hs.ac.kr>
2022-08-25 18:24:21 +0900

    pg 12944 평균 구하기

---
+++ Algorithm/programmers/pg_12944.swift
@@ -0,0 +1,13 @@
+//
+//  pg_12944.swift
+//  Algorithm
+//
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12944
+//  평균 구하기
+
+import Foundation
+
+func solution(_ arr:[Int]) -> Double {
+    let sum = arr.reduce(0, {$0 + $1} )
+    return (Double(sum) / Double(arr.count))
+}
