6538b2e7a7ef1ec321b90080f143c5f8c7ff8486
cchanmi <gkfn980@hs.ac.kr>
2022-08-29 23:39:50 +0900

    pg 12937 짝수와 홀수

---
+++ Algorithm/programmers/pg_12937.swift
@@ -0,0 +1,13 @@
+//
+//  pg_12937.swift
+//  Algorithm
+//
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12937
+//  짝수와 홀수
+
+import Foundation
+
+func solution(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
+
