d830317d085d82d21c8c7ccf87f49c95315316ce
cchanmi <gkfn980@hs.ac.kr>
2022-09-02 17:53:32 +0900

    pg 12934 정수 제곱근 판별

--- Algorithm/programmers/pg_12934.swift
+++ Algorithm/programmers/pg_12934.swift
@@ -11,14 +11,3 @@ func solution(_ n:Int64) -> Int64 {
     let number = Int64(sqrt(Double(n)))
     return (number * number) == n ? (number+1)*(number+1) : -1
 }
-
-
-/*
- import Foundation
-
- func solution(_ n:Int64) -> Int64 {
-     let number = sqrt(Double(n))
-     return Int64(pow(number, 2)) == n ? Int64(pow(number+1, 2)) : -1
- }
- 왜 틀렸을까?
- */
