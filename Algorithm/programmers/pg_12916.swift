dfb4840888d66fffeb241cbf84eda884adbf59ae
cchanmi <gkfn980@hs.ac.kr>
2022-09-10 20:45:41 +0900

    pg 12916 문자열 내 p와 y의 개수

---
+++ Algorithm/programmers/pg_12916.swift
@@ -0,0 +1,27 @@
+//
+//  pg_12916.swift
+//  Algorithm
+//
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12916
+//  문자열 내 p와 y의 개수
+
+import Foundation
+
+func solution(_ s:String) -> Bool {
+    var pCount = 0
+    var yCount = 0
+    
+    for i in s {
+        if i == "p" || i == "P" {
+            pCount += 1
+        } else if i == "y" || i == "Y" {
+            yCount += 1
+        }
+    }
+    
+    return (pCount == yCount) ? true : false
+}
+
+/*
+ 0.01ms / 16.4 MB
+ */
