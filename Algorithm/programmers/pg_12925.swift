51ec8e338182777ae89400c2aa47a6d0ebf8007b
cchanmi <gkfn980@hs.ac.kr>
2022-09-18 20:42:30 +0900

    pg 12925 문자열을 정수로 바꾸기

---
+++ Algorithm/programmers/pg_12925.swift
@@ -0,0 +1,18 @@
+//
+//  pg_12925.swift
+//  Algorithm
+//
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12925
+//  문자열을 정수로 바꾸기
+
+import Foundation
+
+func solution(_ s:String) -> Int {
+    return Int(s)!
+}
+
+print(solution("-1234"))
+
+/*
+ 0.03ms / 16.3MB
+ */
